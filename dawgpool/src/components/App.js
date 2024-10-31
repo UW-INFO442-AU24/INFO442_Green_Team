5066f221c8608afce4f84f51d87b991a29c18711
keki030215 <keliuhan215@gmail.com>
2024-10-31 13:09:27 -0700

    fix minor issue and delete redundant files

--- dawgpool/src/components/App.js
+++ dawgpool/src/components/App.js
@@ -3,21 +3,21 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import { LoginPage } from './Login.js';
 import MainPage from './MainPage.js';
 import { Navbar } from './NavBar.js';
-import Profile from './profile.js'
+import Profile from './Profile.js'
 
 
 function App(props) {
   const location = useLocation();
 
-  const isLoginPage = location.pathname === '/';
+  const isLoginPage = location.pathname === "/login";
 
   return (
     <div>
       {!isLoginPage && <Navbar />}
 
       <Routes>
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/n" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
