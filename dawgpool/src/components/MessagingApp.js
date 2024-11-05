49e9cf4cfee4c97fe44186c13e233610065e8644
SammyBharadwaj <sammyb56@outlook.com>
2024-11-05 14:40:17 -0800

    merged my css

--- dawgpool/src/components/MessagingApp.js
+++ dawgpool/src/components/MessagingApp.js
@@ -1,7 +1,7 @@
 // src/components/MessagingApp.js
 import React, { useState } from 'react';
-import Sidebar from './sidebar';
-import ChatView from './chatview.js';
+import Sidebar from './Sidebar'; // Make sure "Sidebar" is uppercase
+import ChatView from './ChatView'; // Make sure "ChatView" is in PascalCase
 
 const MessagingApp = () => {
   const [selectedChat, setSelectedChat] = useState(null);
@@ -15,17 +15,13 @@ const MessagingApp = () => {
 
   return (
     <div>
-
       <button id="toggle-sidebar" className="btn btn-primary d-lg-none">Toggle Sidebar</button>
       <div className="messaging-container">
         <Sidebar chats={chats} onSelectChat={handleChatSelect} />
-        <Chatview selectedChat={selectedChat} />
+        <ChatView selectedChat={selectedChat} /> {/* Use ChatView with capital "V" */}
       </div>
     </div>
   );
 };
 
 export default MessagingApp;
-
-
-
