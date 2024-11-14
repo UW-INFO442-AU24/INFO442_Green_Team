2348e069dad80b22de933b53a349ac9035411043
keatonstaggs <114716233+keatonstaggs@users.noreply.github.com>
2024-11-14 14:33:58 -0800

    fixed chatview

--- dawgpool/src/components/MessagingApp.js
+++ dawgpool/src/components/MessagingApp.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Sidebar from './Sidebar.js';
-import ChatView from './ChatView.js';
+import Chatview from './Chatview.js';
 
 const MessagingApp = () => {
   const [selectedChat, setSelectedChat] = useState(null);
@@ -17,7 +17,7 @@ const MessagingApp = () => {
       <button id="toggle-sidebar" className="btn btn-primary d-lg-none">Toggle Sidebar</button>
       <div className="messaging-container">
         <Sidebar chats={chats} onSelectChat={handleChatSelect} />
-        <ChatView selectedChat={selectedChat} />
+        <Chatview selectedChat={selectedChat} />
       </div>
     </div>
   );
